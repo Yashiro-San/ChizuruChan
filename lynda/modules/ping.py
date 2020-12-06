dcf04e3703e8998d552f2eb0de2b97e612b61bdd
Zero <69114010+Anomaliii@users.noreply.github.com>
2020-12-06 10:02:39 +0700

    Update ping.py

--- lynda/modules/ping.py
+++ lynda/modules/ping.py
@@ -73,7 +73,7 @@ def ping(bot: Bot, update: Update):
     uptime = get_readable_time((time.time() - StartTime))
 
     reply_msg = ("PONG!!\n"
-                 "<b>Time taken:</b> <code>{}</code>\n"
+                 "<b>Time Taken:</b> <code>{}</code>\n"
                  "<b>Service uptime:</b> <code>{}</code>".format(telegram_ping, uptime))
 
     update.effective_message.reply_text(reply_msg, parse_mode=ParseMode.HTML)
